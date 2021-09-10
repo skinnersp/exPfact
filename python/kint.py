117a5cf6d35d1162ea389ee262e301ea84fe9059
Michele Stofella <bsms@leeds.ac.uk>
2021-09-11 00:05:10 +0100

    Contant values corrected

--- python/kint.py
+++ python/kint.py
@@ -15,10 +15,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 from math import *
-from constants import *
+from constants_HD import *
 import numpy as np
-
-
 
 def calculate_kint_for_sequence(first_residue, last_residue, seq, temperature, pH):
     prolines = []
@@ -36,8 +34,6 @@ def calculate_kint_for_sequence(first_residue, last_residue, seq, temperature, p
                prolines.append(first_residue + jj)
            else:
                kint[assignment-1] = calculate_kint_per_residue(res1,res,assignment,len(seq), temperature, pH)
-        print("**",assignment,len(seq))
-        print("***",kint[assignment-1])
         jj += 1
         res1=res
     print("Residue\tkint")
@@ -53,36 +49,39 @@ def calculate_kint_per_residue(residue1, residue2, num, length, temperature, pH)
 
     lamb1 = acid(residue2, temperature, pH, "lamb")
     rho1 = acid(residue1, temperature, pH, "rho")
-    
     if num == 2: rho1 += rho_Nterm_acid
-    elif (num==length):lamb1=lamb1+lamb_Cterm_acid
-    
+    elif (num==length) : lamb1 += lamb_Cterm_acid
     Fa = 10**(lamb1+rho1)
+    
     lamb2 = base(residue2, temperature, pH, "lamb")
     rho2 = base(residue1, temperature, pH, "rho")
-    
     if num == 2: rho2 += +rho_Nterm_base
-    elif (num==length):lamb2=lamb2+lamb_Cterm_base
-
+    elif (num==length): lamb2 += lamb_Cterm_base
     Fb = 10**(lamb2+rho2)
     
-    kint = Fa * ka * get_D(pH) * get_Fta(temperature) * 60 + Fb * kb * get_OD(pH) * get_Ftb(temperature) * 60 + Fb * \
-           kw * get_Ftw(temperature) * 60
+    t1 = Fa * ka * get_D(pH)  * get_Fta(temperature) * 3600
+    t2 = Fb * kb * get_OD(pH) * get_Ftb(temperature) * 3600
+    t3 = Fb * kw * get_Ftw(temperature) * 3600
+    
+    print(lamb1,rho1,lamb2,rho2,Fa,Fb,t1,t2,t3)
+    kint = Fa * ka * get_D(pH)  * get_Fta(temperature) * 3600 +\
+           Fb * kb * get_OD(pH) * get_Ftb(temperature) * 3600 +\
+           Fb * kw * get_Ftw(temperature) * 3600
 
-    return kint	
+    return kint	# in hr-1
 
 
 def acid(residue, temperature, pH, value):
 
     if residue == "H":
-        lamb = log10(10**(-0.8-pH)/(10**(-get_pK_his(temperature))+10**(-pH))+10**(-get_pK_his(temperature))/(10**(-get_pK_his(temperature))+10**(-pH)))
-        rho = log10(10**(-0.51-pH)/(10**(-get_pK_his(temperature))+10**(-pH))+10**(-get_pK_his(temperature))/(10**(-get_pK_his(temperature))+10**(-pH)))			
+        lamb = log10(10**(-0.80-pH)/(10**(-get_pK_his(temperature))+10**(-pH))+10**( 0.00-get_pK_his(temperature))/(10**(-get_pK_his(temperature))+10**(-pH)))
+        rho =  log10(10**(-0.51-pH)/(10**(-get_pK_his(temperature))+10**(-pH))+10**( 0.00-get_pK_his(temperature))/(10**(-get_pK_his(temperature))+10**(-pH)))			
     elif residue == "D":
-        lamb = log10(10**(-0.9-pH)/(10**(-get_pK_asp(temperature))+10**(-pH))+10**(0.9-get_pK_asp(temperature))/(10**(-get_pK_asp(temperature))+10**(-pH)))			    
-        rho = log10(10**(-0.12-pH)/(10**(-get_pK_asp(temperature))+10**(-pH))+10**(0.58-get_pK_asp(temperature))/(10**(-get_pK_asp(temperature))+10**(-pH)))			   	
+        lamb = log10(10**(-0.90-pH)/(10**(-get_pK_asp(temperature))+10**(-pH))+10**( 0.90-get_pK_asp(temperature))/(10**(-get_pK_asp(temperature))+10**(-pH)))			    
+        rho =  log10(10**(-0.12-pH)/(10**(-get_pK_asp(temperature))+10**(-pH))+10**( 0.58-get_pK_asp(temperature))/(10**(-get_pK_asp(temperature))+10**(-pH)))			   	
     elif residue == "E":
-        lamb = log10(10**(-0.6-pH)/(10**(-get_pK_glu(temperature))+10**(-pH))+10**(-0.9-get_pK_glu(temperature))/(10**(-get_pK_glu(temperature))+10**(-pH)))			    
-        rho = log10(10**(-0.27-pH)/(10**(-get_pK_glu(temperature))+10**(-pH))+10**(0.31-get_pK_glu(temperature))/(10**(-get_pK_glu(temperature))+10**(-pH)))			   	
+        lamb = log10(10**(-0.60-pH)/(10**(-get_pK_glu(temperature))+10**(-pH))+10**(-0.90-get_pK_glu(temperature))/(10**(-get_pK_glu(temperature))+10**(-pH)))			    
+        rho =  log10(10**(-0.27-pH)/(10**(-get_pK_glu(temperature))+10**(-pH))+10**( 0.31-get_pK_glu(temperature))/(10**(-get_pK_glu(temperature))+10**(-pH)))			   	
     else:
         lamb = para[residue][0]
         rho = para[residue][1]
@@ -91,18 +90,17 @@ def acid(residue, temperature, pH, value):
     elif value == "rho":
         return rho
 
-
 def base(residue, temperature, pH, value):
 
     if residue == "H":
-        lamb = log10(10**(0.8-pH)/(10**(-get_pK_his(temperature))+10**(-pH))+10**(-0.1-get_pK_his(temperature))/(10**(-get_pK_his(temperature))+10**(-pH)))
-        rho = log10(10**(0.83-pH)/(10**(-get_pK_his(temperature))+10**(-pH))+10**(0.14-get_pK_his(temperature))/(10**(-get_pK_his(temperature))+10**(-pH)))
+        lamb = log10(10**( 0.80-pH)/(10**(-get_pK_his(temperature))+10**(-pH))+10**(-0.10-get_pK_his(temperature))/(10**(-get_pK_his(temperature))+10**(-pH)))
+        rho =  log10(10**( 0.83-pH)/(10**(-get_pK_his(temperature))+10**(-pH))+10**( 0.14-get_pK_his(temperature))/(10**(-get_pK_his(temperature))+10**(-pH)))
     elif residue == "D":
-        lamb = log10(10**(0.69-pH)/(10**(-get_pK_asp(temperature))+10**(-pH))+10**(0.1-get_pK_asp(temperature))/(10**(-get_pK_asp(temperature))+10**(-pH)))
-        rho = log10(10**(0.6-pH)/(10**(-get_pK_asp(temperature))+10**(-pH))+10**(-0.18-get_pK_asp(temperature))/(10**(-get_pK_asp(temperature))+10**(-pH)))
+        lamb = log10(10**( 0.69-pH)/(10**(-get_pK_asp(temperature))+10**(-pH))+10**( 0.10-get_pK_asp(temperature))/(10**(-get_pK_asp(temperature))+10**(-pH)))
+        rho =  log10(10**( 0.60-pH)/(10**(-get_pK_asp(temperature))+10**(-pH))+10**(-0.18-get_pK_asp(temperature))/(10**(-get_pK_asp(temperature))+10**(-pH)))
     elif residue == "E":
-        lamb = log10(10**(0.24-pH)/(10**(-get_pK_glu(temperature))+10**(-pH))+10**(-0.11-get_pK_glu(temperature))/(10**(-get_pK_glu(temperature))+10**(-pH)))
-        rho = log10(10**(-0.39-pH)/(10**(-get_pK_glu(temperature))+10**(-pH))+10**(-0.15-get_pK_glu(temperature))/(10**(-get_pK_glu(temperature))+10**(-pH)))
+        lamb = log10(10**( 0.24-pH)/(10**(-get_pK_glu(temperature))+10**(-pH))+10**(-0.11-get_pK_glu(temperature))/(10**(-get_pK_glu(temperature))+10**(-pH)))
+        rho =  log10(10**( 0.39-pH)/(10**(-get_pK_glu(temperature))+10**(-pH))+10**(-0.15-get_pK_glu(temperature))/(10**(-get_pK_glu(temperature))+10**(-pH)))
     else:
         lamb = para[residue][2]
         rho = para[residue][3]
