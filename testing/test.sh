e91d89640ab0344b8cb4f23a723a0d5f19b383ce
Michele Stofella <bsms@leeds.ac.uk>
2021-09-15 18:26:02 +0100

    deleted useless input files

--- testing/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in `seq 1 1 100` ; do
-
-python3 ~/local/exPfact-master_seq/python/exPfact.py --temp 300 --pH 7.0  --dexp test.Dexp --ass test.list --weights test.weights --tol 1e-11 --rand 10000 --seq test.seq --out out$i
-
-done
-
-
